453aa31f782c45179ca984ed9465bc1a4e58f124
mary <maryv2@yandex.ru>
2019-07-01 18:47:42 +0300

    add change for hw4

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -4,6 +4,9 @@ import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
 
+    private SecurityUtil() {
+    }
+
     public static int authUserId() {
         return 1;
     }
